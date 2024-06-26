051ff85ff2f7475edd9f21ebd9af1332745637cc
PieroM1 <u201923446@upc.edu.pe>
2024-06-26 09:51:08 -0500

    update

--- src/main/java/com/construtech/buildsphere/platform/BuildspherePlatformApplication.java
+++ src/main/java/com/construtech/buildsphere/platform/BuildspherePlatformApplication.java
@@ -24,18 +24,5 @@ public class BuildspherePlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(BuildspherePlatformApplication.class, args);
     }
-    @Configuration
-    public static class Myconfiguration{
-        @Bean
-        public WebMvcConfigurer corsConfigurer(){
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-                }
-            };
-        }
-    }
 
 }
